9f461dd03dc3139844f0357da7862cfb905a01f0
Attanayake Perera <Udara@Attanayakes-MacBook-Pro.local>
2017-04-10 00:43:06 +0800

    job controler

--- src/main/java/com/ntu/ip/controller/JobSearchController.java
+++ src/main/java/com/ntu/ip/controller/JobSearchController.java
@@ -4,37 +4,43 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.mail.Session;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.ntu.ip.dto.JobDto;
-import com.ntu.ip.model.Job;
 import com.ntu.ip.service.JobService;
 
 @WebServlet(urlPatterns = "/jobSearch.do")
 public class JobSearchController extends HttpServlet {
 
+	private static final String SKILLS2 = "skills";
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -8341782287640462738L;
 	private JobService jobService = new JobService();
 	
-//	protected void doGet(HttpServletRequest request,
-//			HttpServletResponse response) throws ServletException, IOException {
-//		request.getRequestDispatcher("/WEB-INF/views/jobs.jsp").forward(
-//				request, response);
-//	}
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		session.setAttribute(SKILLS2, request.getParameter(SKILLS2));
+		request.getRequestDispatcher("/WEB-INF/search.jsp").forward(
+				request, response);
+		
+	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String skills = request.getParameter("skills");
+		String skills = (String) request.getSession().getAttribute(SKILLS2);
 		List<JobDto> jobList = new ArrayList<>();
 		if(skills == null){
 			jobList.addAll(jobService.getLatestJobs());
@@ -49,8 +55,7 @@ public class JobSearchController extends HttpServlet {
 		response.setContentType("application/json");
 		response.setCharacterEncoding("UTF-8");
 		response.getWriter().print(jsonArray);
-		request.getRequestDispatcher("/WEB-INF/search.jsp").forward(
-				request, response);
+		
 	}
 
 }
