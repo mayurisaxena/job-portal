f8752c92dada24a227910db805d7425238ca116d
Attanayake Perera <Udara@Attanayakes-MacBook-Pro.local>
2017-04-09 16:48:31 +0800

    dto

---
+++ src/main/java/com/ntu/ip/dto/JobDto.java
@@ -0,0 +1,76 @@
+package com.ntu.ip.dto;
+
+import java.text.SimpleDateFormat;
+
+import com.ntu.ip.model.Job;
+
+public class JobDto {
+
+	private int jobId;
+	private String title;
+	private String description;
+	private String skills;
+	private String postedDate;
+	private String employer;
+	
+	
+	private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getSkills() {
+		return skills;
+	}
+
+	public void setSkills(String skills) {
+		this.skills = skills;
+	}
+
+	public String getPostedDate() {
+		return postedDate;
+	}
+
+	public void setPostedDate(String postedDate) {
+		this.postedDate = postedDate;
+	}
+
+	public String getEmployer() {
+		return employer;
+	}
+
+	public void setEmployer(String employer) {
+		this.employer = employer;
+	}
+	
+	public JobDto(Job job) {
+		this.jobId = job.getId();
+		this.title = job.getTitle();
+		this.description = job.getDescription();
+		this.skills = job.getSkills().stream().map(e->e.getSkillName()).reduce("", String::concat);
+		this.postedDate = dateFormat.format(job.getPostedDate());
+		this.employer = job.getEmployer().getAlias();
+	}
+
+	public int getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(int jobId) {
+		this.jobId = jobId;
+	}
+
+}
