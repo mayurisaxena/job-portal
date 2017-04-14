2d4c9998032f17514d6626d8e483e8d9cd453739
Attanayake Perera <Udara@Attanayakes-MacBook-Pro.local>
2017-04-14 09:05:18 +0800

    new

---
+++ src/main/java/com/ntu/ip/controller/EmployerController.java
@@ -0,0 +1,47 @@
+package com.ntu.ip.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ntu.ip.model.Employer;
+import com.ntu.ip.service.EmployerService;
+
+@WebServlet(urlPatterns = "/employerAdd.do")
+public class EmployerController extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4720060139357905574L;
+
+	private static final String EMPLOYER = "Employer";
+
+	private EmployerService employerService = new EmployerService();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/employee.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Employer employer = new Employer();
+		employer.setAlias(request.getParameter("aliasname"));
+		employer.setUserName(request.getParameter("email"));
+		employer.setRole(EMPLOYER);
+		employer.setUsrPwd(request.getParameter("password"));
+		employer.setCompanyName(request.getParameter("company"));
+		employer.setContactNo(request.getParameter("number"));
+		employer.setAddress(request.getParameter("Address"));
+		//employer.setDescription(request.getParameter("Description"));
+		employerService.save(employer);
+		response.setStatus(1);
+
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/EmployerHeader.jsp
@@ -0,0 +1,30 @@
+<header>
+	<nav class="navbar navbar-default navbar-fixed-top header-other"
+		role="navigation">
+		<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse"
+					data-target="#bs-example-navbar-collapse-1">
+					<span class="fa fa-bars fa-lg"></span>
+				</button>
+				<a class="navbar-brand" href="../home.jsp"> <img
+					src="img/freeze/logo.png" alt="" class="logo">
+				</a>
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse"
+				id="bs-example-navbar-collapse-1">
+
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="EmployerHome.do">Candidate Search</a></li>
+					<li><a href="EmployerUpdate.do">Update Profile</a></li>
+					<!-- 					<li><a href="canApplications.do">View Applications</a></li> -->
+					<li><a href="logout.do">Logout</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+</header>
